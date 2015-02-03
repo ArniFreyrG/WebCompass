a89560382b076c91fa5da70f61f5766be0f6d250
ArniFreyrG <arnifreyrg@gmail.com>
2015-02-03 17:05:15 +0000

    Backbone implement, fixed browser conflicts

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-function getDir(lat1, lon1, lat2, lon2){
-	p1 = lat1 * Math.PI / 180;
-	p2 = lat2 * Math.PI / 180;
-	d = (lon2 - lon1) * Math.PI / 180;
-	var y = Math.sin(d)*Math.cos(p2);
-	var x = Math.cos(p1)*Math.sin(p2) - Math.sin(p1)*Math.cos(p2)*Math.cos(d);
-	return -((Math.atan2(x,y) * 180 / Math.PI)-90);
-}
 
 function getCurrentTabUrl(callback) {
   var queryInfo = {
@@ -20,98 +12,30 @@ function getCurrentTabUrl(callback) {
   });
 }
 
-// Search for coordinates from "http://freegeoip.net" (kudos)
-function findCoords(url, callback, errorCallback){
-	var x = new XMLHttpRequest();
-	x.open('GET', url);
-	x.responseType = 'json';
-	x.onload = function() {
-		var response = x.response;
-		if(!response) {
-			errorCallback("EKKERT SVAR")
-			return;
-		}
-		var latitude = response.latitude;
-		var longitude = response.longitude;
-		var country = response.country_name;
-		var city = response.city;
-		if(!latitude){
-			errorCallback("FANN EKKI HNIT");
-		}
-		if(latitude === 65 && longitude === -18){
-			latitude = 64.146;
-			longitude = -21.904;
-		}
-		callback(latitude,longitude,country,city);
-	}
-	x.onerror = function() {
-		errorCallback("NETWORK ERROR")
-	}
-	x.send();
-}
+var urlUni;
 
-var info = {
-	latYou: null,
-	lonYou: null,
-	latThere: null,
-	lonThere: null,
-	angle: null,
-	cityYou: null,
-	countryYou: null,
-	cityThere: null,
-	countryThere: null,
-	url: null,
-	dom: null
-};
-
-chrome.tabs.executeScript(null, {
+chrome.browserAction.onClicked.addListener(function(tab){
+	chrome.tabs.executeScript(null, {
 	file: 'jquery-2.1.3.min.js'}, 
 	function() {chrome.tabs.executeScript(null, {
-		file: "underscore-min.js"},
+		file: 'underscore-min.js'},
 		function() {chrome.tabs.executeScript(null, {
 			file: 'backbone-min.js'},
-			function(){console.log("hér2");
-			chrome.browserAction.onClicked.addListener(function(tab){
 				getCurrentTabUrl(function(url){
-					info.url = url;
 					// regex to find hostname from url
-					dom = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-					info.dom = dom;
-					var searchUrl = "http://freegeoip.net/json/" + dom;
-					info.latThere = searchUrl;
-					findCoords(searchUrl, function(latitude, longitude, country, city){
-						if(!latitude){info.latThere = "BÍÐA AÐEINS!!";}
-						info.latThere = latitude;
-						info.lonThere = longitude;
-						info.countryThere = country;
-						info.cityThere = city
-						// finding you (yes, YOU!)
-						findCoords("http://freegeoip.net/json/", function(latitude2, longitude2, country2, city2){
-							info.latYou = latitude2;
-							info.lonYou = longitude2;
-							info.countryYou = country2;
-							info.cityYou = city2;
-							var direction = getDir(latitude2,longitude2,latitude,longitude);
-							info.angle = direction;
-							chrome.tabs.executeScript({ file: 'contentscript.js' }); 
-							chrome.tabs.insertCSS({ file: 'styles.css'});
-							
-						}, function(errorMessage) {
-					      info.latTere = 'Cannot find you. ' + errorMessage;
-					    });
-					}, function(errorMessage) {
-						info.latThere ='Cannot find website. ' + errorMessage;
-				    });
-					if(!info.latYou){info.latYou = "failed";}
+					urlUni = url;
+					chrome.tabs.executeScript({ file: 'contents.js' }); 
+					chrome.tabs.insertCSS({ file: 'styles.css'});
 				});
 			});
 		});
 	});
 });
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 	    console.log("MESSAGE RECEIVED");
-	    sendResponse(info);
+	    sendResponse({url: urlUni});
 	} 
 );
 
--- content.js
+++ content.js
@@ -1,12 +1,43 @@
+function getDir(lat1, lon1, lat2, lon2){
+	p1 = lat1 * Math.PI / 180;
+	p2 = lat2 * Math.PI / 180;
+	d = (lon2 - lon1) * Math.PI / 180;
+	var y = Math.sin(d)*Math.cos(p2);
+	var x = Math.cos(p1)*Math.sin(p2) - Math.sin(p1)*Math.cos(p2)*Math.cos(d);
+	return -((Math.atan2(x,y) * 180 / Math.PI)-90);
+}
+
+console.log("þar");
+console.log(Backbone.VERSION);
+console.log(jQuery.fn.jquery);
 
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 	console.log("MESSAGE SENT");
 	if(!response){console.log("NO REPLY");}	
     else{ 
-    	info = response;
-    	console.log(JSON.stringify(response, null, 4));
-    	document.head.innerHTML = '<link rel="stylesheet" type="text/css" href="styles.css">';
-    	document.body.innerHTML = '<div id="container"><iframe width="100%" height="100%" src=' + response.url + '></iframe><div id="hidden-container"><div id="needle-container">&#8682;<br>THIS SITE IS <i>THAT</i> WAY!<br><div id="moreinfo"></div><script> function info(){document.getElementById("moreinfo").innerHTML = "hér má sjá meiri upplýsingar"}</script><button id="infobutton" onclick="info()">ýta hér</button></div></div></div>';
-    	document.body.style["-webkit-transform"] = "rotate(" + response.angle + "deg)";
+    	dom = response.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+		var model = Backbone.Model.extend({
+			initialize: function(options){
+				this.page = options.page;
+			},
+			url: function(){
+				return "http://freegeoip.net/json/" + this.page;
+			}
+		});
+		var here = new model({page:""});
+		var there = new model({page: dom});
+		here.fetch({
+			success:function(){
+				there.fetch({
+					success:function(){
+				    	var angle = getDir(here.get("latitude"),here.get("longitude"),there.get("latitude"),there.get("longitude"));
+				    	document.body.innerHTML = '<div id="container"><iframe width="100%" height="100%" src=' + response.url + '></iframe><div id="hidden-container"><div id="needle-container">&#8682;<br>THIS SITE IS <i>THAT</i> WAY!<br><div id="moreinfo"></div><script> function info(){document.getElementById("moreinfo").innerHTML = "hér má sjá meiri upplýsingar"}</script><button id="infobutton" onclick="info()">ýta hér</button></div></div></div>';
+				    	document.body.style["-webkit-transform"] = "rotate(" + angle + "deg)";
+						
+						
+					}
+				});
+			}
+		});	
     }
 });
