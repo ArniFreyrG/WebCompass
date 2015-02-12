052bbcae50c8e0f95bb1cfc01f8b3ca1abcf3c32
Árni Freyr Gunnarsson <arnifreyrg@gmail.com>
2015-02-12 12:52:47 +0000

    Info window working

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// Get bearing from coordinates
 function getDir(lat1, lon1, lat2, lon2){
 	p1 = lat1 * Math.PI / 180;
 	p2 = lat2 * Math.PI / 180;
@@ -5,19 +6,6 @@ function getDir(lat1, lon1, lat2, lon2){
 	var y = Math.sin(d)*Math.cos(p2);
 	var x = Math.cos(p1)*Math.sin(p2) - Math.sin(p1)*Math.cos(p2)*Math.cos(d);
 	return -((Math.atan2(x,y) * 180 / Math.PI)-90);
-}
-
-function getMid(lat1, lng1, lat2, lng2){
-	var a = [0,0];
-	d = (lng2-lng1) * Math.PI / 180;
-	p1 = lat1 * Math.PI / 180;
-	p2 = lat2 * Math.PI / 180;
-	var x = Math.cos(p2) * Math.cos(d);
-	var y = Math.cos(p2) * Math.sin(d);
-	var p3 = Math.atan2(Math.sin(p1) + Math.sin(p2), Math.sqrt((Math.cos(p1)+x)*(Math.cos(p1)+x) + y*y));
-	var d2 = lng1 + Math.atan2(y, Math.cos(p1)+x);
-	var a = [p3,d2];
-	return a;
 }
 
 var model = Backbone.Model.extend({
@@ -29,7 +17,6 @@ var model = Backbone.Model.extend({
 	}
 });
 
-console.log("þar");
 console.log(Backbone.VERSION);
 console.log(jQuery.fn.jquery);
 
@@ -37,45 +24,70 @@ chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 	console.log("MESSAGE SENT");
 	if(!response){console.log("NO REPLY");}	
     else{ 
-		dom = response.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-		var here = new model({page:""});
-		var there = new model({page: dom});
-		here.fetch({
-			success:function(){
-				there.fetch({
-					success:function(){
-				    	var angle = getDir(here.get("latitude"),here.get("longitude"),there.get("latitude"),there.get("longitude"));
-				    	var rotatorHTML = chrome.extension.getURL("rotator.html");
-				    	console.log(rotatorHTML);
-				    	$("body").load(rotatorHTML,function(){
-				    		$("iframe").attr({"src": response.url});
-				    		console.log(response.url);
-					    	setTimeout(function(){
-				    			$("iframe, #hidden-container").animate( {borderSpacing: angle}, {step: function(now,fx){
-					    			$(this).css({"-webkit-transform": 'rotate(' + now + 'deg)'}, "fast");
-					    			},
-					    			duration: 7000
-					    		}), "linear";
-				    		}, 500);
-					    	$('#blah').css({height:'20px', overflow:'hidden'});
-							$('#blah').on('click', function() {
-							    var $this = $(this);
-							    if ($this.data('open')) {
-							        $this.animate({height:'20px'});
-							        $this.data('open', 0);
+    	var loadingHTML = chrome.extension.getURL("loading.html");
+    	var wheelurl = chrome.extension.getURL("loader2.gif");
+    	$("body").attr({"scrolling": "no"})
+    	$("body").load(loadingHTML,function(){
+    		$("head").html("");
+    		jQuery("#background").css("background-image", "url(" + response.imgsrc + ")");
+    		$("img").attr({"src": wheelurl});
+			dom = response.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+			var here = new model({page:""});
+			var there = new model({page: dom});
+			here.fetch({
+				success:function(){
+					there.fetch({
+						success:function(){
+					    	var angle = getDir(here.get("latitude"),here.get("longitude"),there.get("latitude"),there.get("longitude"));
+					    	var rotatorHTML = chrome.extension.getURL("rotator.html");
+					    	console.log(rotatorHTML);
+					    	$("body").load(rotatorHTML,function(){
 
-							    }
-							    else {
-							        $this.animate({height:'300px'});
-							        $this.data('open', 1);
-							    }
-							});
-				    		});
-					}
-				});
-			}
+					    		// Animate page turning
+					    		$("iframe").attr({"src": response.url});
+					    		setTimeout(function(){
+					    			$("iframe, #hidden-container").animate( {borderSpacing: angle}, {step: function(now,fx){
+						    			$(this).css({"-webkit-transform": 'rotate(' + now + 'deg)'}, "fast");
+						    			},
+						    			duration: 7000
+						    		}), "linear";
+					    		}, 500);
+
+					    		// Write location text
+					    		$('#cityHere').text(here.get("city"));
+					    		$('#countryHere').text(here.get("country_name"));
+					    		$('#latHere').text(here.get("latitude"));
+					    		$('#lngHere').text(here.get("longitude"));
+					    		$('#there').text(dom + " location");
+					    		if(there.get("city"))$('#cityThere').text(there.get("city"));
+					    		$('#countryThere').text(there.get("country_name"));
+					    		$('#latThere').text(there.get("latitude"));
+					    		$('#lngThere').text(there.get("longitude"));
+
+					    		// more/less info animation
+						    	$('#tables').css({height:'17px', overflow:'hidden'});
+								$('#info').on('click', function() {
+									var $this = $(this);
+								    var $tables = $('#tables');
+								    if ($tables.data('open')) {
+								        $tables.animate({height:'17px'});
+								        $tables.data('open', 0);
+								        $this.text('more info');
+
+								    }
+								    else {
+								        $tables.animate({height:'210px'});
+								        $tables.data('open', 1);
+								        $this.text('less info');
+								    }
+								});
+					    		});
+						}
+					});
+				}
+			});
 		});	
-    	}
+	}
 });
 
 
